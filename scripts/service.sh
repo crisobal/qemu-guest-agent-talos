b3a52797f5c152918ed6d0cbab1c257a3a8e0086
Crispin Tschirky <crispin@tschirky.ch>
2023-02-27 17:32:26 +0100

    fix service.sh header

--- scripts/service.sh
+++ scripts/service.sh
@@ -1,4 +1,11 @@
-!#/bin/bash
+#!/bin/sh
+# (c) 2023 by Crispin Tschirky <crispin.tschirky@gmail.com>
+#
+# Licensed under BSD-3 License, see https://opensource.org/licenses/BSD-3-Clause
+#
+# Replacement for shutdown on pod which invokes the operation on the real host
+# can be used for qemu-guest-agent daemon pod to reboot the host in case e.g. proxmox requests it
+#
 
 LOGFILE=/var/log/qemu-ga.log
 
